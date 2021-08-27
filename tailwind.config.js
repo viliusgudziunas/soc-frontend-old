f206aea66dd94a994930c08d8761f9205690817f
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2021-08-28 00:27:12 +0300

    Initial tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  plugins: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: { extend: {} },
+  variants: { extend: {} },
+};
