dca0de998dfd643768720f3a0ad0d252737f72fa
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2021-09-30 23:14:46 +0300

    Trying to deploy: 3

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,22 @@
+const path = require('path');
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
+  },
+
+  webpack: {
+    alias: {
+      components: path.resolve(__dirname, './src/components/'),
+      contexts: path.resolve(__dirname, './src/contexts/'),
+      lib: path.resolve(__dirname, './src/lib/'),
+      models: path.resolve(__dirname, './src/models/'),
+      pages: path.resolve(__dirname, './src/pages/'),
+      services: path.resolve(__dirname, './src/services/'),
+      shared: path.resolve(__dirname, './src/shared/'),
+    },
+    extensions: ['.ts', '.tsx'],
   },
 };
